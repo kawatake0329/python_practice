eb8d340e350d1cf89c323b53a4e3a3d9e7e7267d
KawaTake <104962638+kawatake0329@users.noreply.github.com>
2022-12-02 23:25:20 +0900

    12月02日

--- 04_制御フローとコード構造/39.lesson.py
+++ 04_制御フローとコード構造/39.lesson.py
@@ -1,6 +1,23 @@
 # while文とcontinue文とbreak文
 count = 0
 
+# countが5を超えるとシステムがストップする
 while count < 5:
+    print(count)
+    count += 1
+# そのため、一つ上の行countを間違った形にしてしまうと無限ループが起こる
+
+# break文を使った方法
+count = 1
+while True:
+    if count >= 5:
+        break
+    
+    if count == 2:
+        count += 1
+        continue
+#上記のようにbreakではなくcontinueを使うとその処理だけ飛ばしてくれるようになる
+# 今回は2の時表示をせず次の3から表示されるようになる。 
+
     print(count)
     count += 1
