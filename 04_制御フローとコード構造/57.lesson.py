5836afb6dd01db8de6cbe0c6b9d95c0d04cc5dda
KawaTake <104962638+kawatake0329@users.noreply.github.com>
2023-01-07 23:45:43 +0900

    01月07日

--- 04_制御フローとコード構造/57.lesson.py
+++ 04_制御フローとコード構造/57.lesson.py
@@ -1,1 +1,39 @@
 # デコレーター
+
+def print_more(func):
+    def wrapper(*args, **kwargs):
+        print('func:', func.__name__)
+        print('atgs:', args)
+        print('kwargs:', kwargs)
+        result = func(*args, **kwargs)
+        pritn('result:', result)
+        return result
+    return wrapper
+
+def print_info(func):
+    def wrapper(*args, **kwargs):
+        print('start')
+        result = func(*args, **kwargs)
+        pritn('end')
+        return result
+    return wrapper
+
+@print_info
+@print_more
+def add_num(a, b):
+    return a + b
+
+r = add_num(10, 20)
+print(r)
+
+f = print_info(add_num)
+r = f(10, 20)
+print(r)
+
+print('start')
+r = add_num(10, 20)
+print('end')
+
+print(r)
+
+# デバッカを使って確認必須！
